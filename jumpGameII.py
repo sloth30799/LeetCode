912af0a63d443c41b981ff7d8811bbeb1e5e7ebb
Han Ye Htun <HanYeHtun@fusionsol.com>
2024-12-13 14:19:39 -0800

    jump game 2

---
+++ jumpGameII.py
@@ -0,0 +1,41 @@
+# You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+# Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+# 0 <= j <= nums[i] and
+# i + j < n
+# Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+
+# Example 1:
+
+# Input: nums = [2,3,1,1,4]
+# Output: 2
+# Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+# Example 2:
+
+# Input: nums = [2,3,0,1,4]
+# Output: 2
+
+def jump(nums: list[int]) -> int:
+    steps = 0
+    farthest = 0
+    current_end = 0
+
+    for i in range(len(nums)):
+        farthest = max(farthest, i + nums[i])
+
+        if i == current_end:
+            if i != len(nums) - 1:
+                steps += 1
+                current_end = farthest 
+                if current_end >= len(nums) - 1:
+                    return steps
+            else:
+                break
+
+    return steps
+
+
+nums = [0]
+print(jump(nums))
